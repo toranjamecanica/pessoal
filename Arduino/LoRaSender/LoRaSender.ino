0a7c56b6d2e8666fdf80abc708b3679f95bfb3f9
pedro fernandes dos santos <pedro_7894@live.com>
2022-04-14 17:39:22 -0300

    yo2

--- Arduino/LoRaSender/LoRaSender.ino
+++ Arduino/LoRaSender/LoRaSender.ino
@@ -4,44 +4,68 @@
 #define BOTAOAZUL 13
 #define LED 25
 
-byte Master = 0xFF;
+// criando IDs unicas para cada loras
+byte Master = 0xFF; 
 byte Node1 = 0xCC;
 byte Node2 = 0xBB;
-String mensage = "" ;
+
+String mensage = "" ; // string pra salvar mensagens
+String comando; // string para salvar o comando do monitor serial
+int Status = 0; // gardar status do comando
 
 
 
 void setup() {
-
-  Heltec.begin(true,true,true,true,BAND);
-  //Serial.begin(9600);
-
+  Heltec.begin(true,true,true,true,BAND); // configurando biblioteca healtec
+  Serial.println("Sistema dessaramdo digite ARMAR para ativar");
 }
+
+
 
 void loop() {
 
-  Serial.println(digitalRead(BOTAOAZUL));
-
-  if(digitalRead(BOTAOPRETO) == 1)
-  {
-    sendPack("On", Master, Node1);
-    //Serial.println(digitalRead(BOTAOPRETO));
+  if(Serial.available()){ // retorna a quantidade de bites disponivel na porta serial e ao final da string retorna 0
+    
+    comando = Serial.readStringUntil('\n'); // salvar a string ate o carecter de final de linha
+    
+    if(comando.equals("ARMAR")) // compara strings se forem iguais retorna 1 
+    {
+      Status = 1; // altera o status do comando se armado status = 1
+      Serial.println("Sistema Armado caso queira desarmar digite DERSAMAR");
+      digitalWrite(25,HIGH); // acende o led do esp quando armado
+      
+    }
+    else if(comando.equals("DESARMAR"))
+    {
+      Status = 0;  // altera o status do comando se desarmado status = 0
+      Serial.println("Sistema dessaramdo digite ARMAR para ativar");
+      digitalWrite(25,LOW); // apaga o led do esp quando desarmado
+    }
+    else{
+      Serial.println("Comdando Ivalido"); // caso errar o comando retorna mensagem 
+    }
   }
 
-  if(digitalRead(BOTAOAZUL) == 1)
+  if(digitalRead(BOTAOPRETO) == 1 && Status == 1)// tanto o botão deve estar precionado quanto o  status deve estar = 1 
+  {
+    sendPack("On", Master, Node1);// chama a função que envia o pacote sendPack(String mensage, byte master, byte node)
+    Serial.println("Preto on"); // pinta o staus do botão no monitor serial 
+  }
+
+  if(digitalRead(BOTAOAZUL) == 1 && Status == 1)
   {
     sendPack("On", Master, Node2);
-    Serial.println(digitalRead(BOTAOAZUL));
+    Serial.println("Azul on");
   }
 
 }
 
 void sendPack(String mensage, byte master, byte node)
 {
-  LoRa.beginPacket();
-  LoRa.write(node);
-  LoRa.write(master);
-  LoRa.write(mensage.length());
-  LoRa.print(mensage);
-  LoRa.endPacket();
+  LoRa.beginPacket(); //inicia o pacote 
+  LoRa.write(node); //envia o id do lora de destino
+  LoRa.write(master); //envia o id do lora master
+  LoRa.write(mensage.length()); //envia o tamanho da mensagem
+  LoRa.print(mensage); // envia a mensagem
+  LoRa.endPacket(); //finaliza o pacote
 }
--- Arduino/LoraReceptor2/LoraReceptor2.ino
+++ Arduino/LoraReceptor2/LoraReceptor2.ino
@@ -3,52 +3,57 @@
 #define BOTAO 12
 #define LED 25
 
+// criando IDs unicas para cada loras
 byte Master = 0xFF;
 byte Node1 = 0xCC;
 byte Node2 = 0xBB;
+// string pra salvar mensagens
 String mensage = "" ;
 
 
 
 void setup() {
 
-  Heltec.begin(true,true,true,true,BAND);
+  Heltec.begin(true,true,true,true,BAND);// configurando biblioteca healtec
   
 }
 
 void loop() {
 
 
-  reciver(LoRa.parsePacket());
+  reciver(LoRa.parsePacket());// chama a funçao que recebe e le pacotes 
  
 }
 
 
-void reciver(int packsize) {
+void reciver(int packsize) { 
 
-  if(packsize == 0) return;
+  if(packsize == 0) return;//se não ha pacotes para receber, retorna
 
-  int remetente = LoRa.read();
-  byte  sender = LoRa.read();
-  byte  msgId = LoRa.read();
 
-  String incomig = "";
+  int remetente = LoRa.read(); // salva o id do remetente
+  byte  sender = LoRa.read();  // salva a id do lora destinatario
+  
+  String incomig = ""; // string para criar mensagem
 
   while (LoRa.available())
   {
-    incomig += (char)LoRa.read();
+    incomig += (char)LoRa.read(); 
   }
 
-  if(remetente != Node1 && remetente != Master) 
+  if(remetente != Node1 && remetente != Master) // o id de rementente e destinatarios não forem iguais retorna 
   {
     Serial.println("Essa mensagem nao é pra mim");
     return;
   }
 
+ //se forem iguais piscar o led do lora 
+
   digitalWrite(25,HIGH);
   delay(100);
   digitalWrite(25,LOW);
 
+ // infos da mensagem no monitor serial
   Serial.println(incomig);
   Serial.println(sender);
   Serial.println(remetente);
